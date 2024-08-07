21e5e6c8f89317ab162c61e8347a0cb3402009b9
haejunejung <jhj9422@unist.ac.kr>
2024-08-07 19:38:16 +0900

    ✨ FEAT   : Setup

---
+++ source/Primitive.d.ts
@@ -0,0 +1,8 @@
+export type Primitive = 
+    | string
+    | number
+    | boolean
+    | bigint
+    | symbol
+    | null
+    | undefined
---
+++ test-d/Primitive.test-d.ts
@@ -0,0 +1,23 @@
+import { expectAssignable, expectNotAssignable } from "tsd";
+import { Primitive } from "../source/Primitive";
+
+// Valid cases where `Primitive` should be assignable
+expectAssignable<Primitive>("string");
+expectAssignable<Primitive>(1);
+expectAssignable<Primitive>(true);
+expectAssignable<Primitive>(9007199254740991n);
+expectAssignable<Primitive>(Symbol('a'));
+expectAssignable<Primitive>(null);
+expectAssignable<Primitive>(undefined);
+
+// Cases where `Primitive` should not be assignable
+expectNotAssignable<Primitive>({});
+expectNotAssignable<Primitive>([]);
+expectNotAssignable<Primitive>(new Date());
+expectNotAssignable<Primitive>(function() {});
+expectNotAssignable<Primitive>(/regex/);
+expectNotAssignable<Primitive>(new Map());
+expectNotAssignable<Primitive>(new Set());
+expectNotAssignable<Primitive>(Promise.resolve());
+expectNotAssignable<Primitive>(new WeakMap());
+expectNotAssignable<Primitive>(new WeakSet());
