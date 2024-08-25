5dee8c5658314572a35187d1147a841060be4bdf
haejunejung <jhj9422@unist.ac.kr>
2024-08-25 21:17:35 +0900

    feat(PickOptional): Add PickOptional type

--- source/utilities/index.ts
+++ source/utilities/index.ts
@@ -1,3 +1,4 @@
+export type { PickOptional } from './PickOptional';
 export type { Simplify } from './Simplify';
 export type { StrictExclude } from './StrictExclude';
 export type { StrictOmit } from './StrictOmit';
