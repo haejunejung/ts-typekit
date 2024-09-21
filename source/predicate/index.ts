a6e78b248031aef16e46dcd4d89e9dac029c77d3
haejunejung <jhj9422@unist.ac.kr>
2024-09-21 21:10:49 +0900

    feat(IsUnion): Add IsUnion type

--- source/predicate/index.ts
+++ source/predicate/index.ts
@@ -3,4 +3,5 @@ export type { IsArray } from './IsArray';
 export type { IsEqual } from './IsEqual';
 export type { IsTuple } from './IsTuple';
 export type { IsNever } from './IsNever';
+export type { IsUnion } from './IsUnion';
 export type { IsUnknown } from './IsUnknown';
